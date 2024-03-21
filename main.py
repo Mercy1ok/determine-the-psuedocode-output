276428cf1984db7ddd48bc276b1841d1f33a8113
hamooda <155935487+Mercy1ok@users.noreply.github.com>
2024-03-21 02:37:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+a = 1
+b = 2
+c = 5
+
+while a < c:
+  a = a + 1
+  b = b + c
+print("test1:")
+print(a, b, c)
+print(                         )
+
+d = 4
+e = 6
+f = 7
+
+while d < f:
+  d = d + 1
+  e = e - 1
+print("test2:")
+print(d, e, f)
+print(                          )
+
+g = 4
+h = 6
+
+while g < h:
+  g = g + 1
+print("test3")
+print(g, h)
+print(                            )
+
+j = 2
+k = 5
+n = 9
+
+print("test 4")
+while j < k:
+  m = 6
+  while m < n:
+    print("goodbye")
+    m = m + 1
+  j = j + 1
+  print(
+       )
+
+print("test 5")
+j =  2
+k = 5
+m = 6
+n = 9
+
+while j < k:
+  while m < n:
+    print("Hello")
+    m = m + 1
+  j = j + 1
+
+  print(
+       )
+
+print("test 6")
+p = 2
+q = 4
+
+while p < q:
+  print("Adios")
+  r = 1
+  while r < q:
+    print("Adios")
+    r = r + 1
+  p = p + 1
